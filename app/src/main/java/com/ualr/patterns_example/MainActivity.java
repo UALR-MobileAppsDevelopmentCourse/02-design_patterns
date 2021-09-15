bc7ce088af1312fb8430c1e55315f11a1a342c4c
irconde <irconde@ualr.edu>
2021-09-15 21:37:45 +0200

    view binding enabled

--- app/src/main/java/com/ualr/patterns_example/MainActivity.java
+++ app/src/main/java/com/ualr/patterns_example/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.ualr.patterns_example.databinding.ActivityMainBinding;
 import com.ualr.patterns_example.model.UserDatabase;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,16 +17,13 @@ public class MainActivity extends AppCompatActivity {
     private UserDatabase usersDB;
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private EditText mIdET;
-    private EditText mNameET;
-    private Button mRetrieveBtn;
-    private Button mSaveBtn;
-    private TextView mOutputUser;
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
 
         // TODO 05: Initialize the model
         usersDB = new UserDatabase();
@@ -37,47 +35,40 @@ public class MainActivity extends AppCompatActivity {
         // Log.d(TAG, String.format("The user with the id %s is %s", user.getId(), user.getName()));
         // TODO 07: Obviously we can check that everything is ok debugging
 
-        // TODO 08: We have to get the references to the text fields in the GUI in order to get data
-        // provided by the user. Check that your textview elements have an id
-        this.mIdET = findViewById(R.id.id_edit_text);
-        this.mNameET = findViewById(R.id.name_edit_text);
-        this.mOutputUser = findViewById(R.id.output_text_view);
 
-        // TODO 09: We have to get a reference of each Button in the text field in order to define the
-        // actions triggered in response to a click action
-        this.mRetrieveBtn = findViewById(R.id.retrieve_btn);
-        // TODO 10: Ctrl + D is very useful. We can duplicate a line
-        this.mSaveBtn = findViewById(R.id.save_btn);
 
-        // TODO 11: We have to define the code that is invoked when the user taps on each button
-        this.mSaveBtn.setOnClickListener(new View.OnClickListener() {
+        // TODO 07: We have to define the code that is invoked when the user taps on each button
+        this.mBinding.saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onSaveBtnClicked();
             }
         });
 
-        // TODO 12: Maybe you don't remember all the methods of a class or interface...
+        // TODO 07.B: Maybe you don't remember all the methods of a class or interface...
         // Don't worry Ctrl + O comes to the rescue
-        this.mRetrieveBtn.setOnClickListener(new View.OnClickListener() {
+        this.mBinding.retrieveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onRetrieveBtnClicked();
             }
         });
+
+        setContentView(mBinding.getRoot());
+
     }
 
     private void onSaveBtnClicked() {
-        String userId = this.mIdET.getText().toString();
-        String userName = this.mNameET.getText().toString();
+        String userId = this.mBinding.idEditText.getText().toString();
+        String userName = this.mBinding.nameEditText.getText().toString();
         if (userId.isEmpty() || userName.isEmpty()) return;
         this.usersDB.addUser(userId, userName);
     }
 
     private void onRetrieveBtnClicked() {
-        String userId = this.mIdET.getText().toString();
+        String userId = this.mBinding.idEditText.getText().toString();
         String userName = this.usersDB.getUser(userId);
-        this.mOutputUser.setText(userName);
+        this.mBinding.outputTextView.setText(userName);
     }
 
 }
